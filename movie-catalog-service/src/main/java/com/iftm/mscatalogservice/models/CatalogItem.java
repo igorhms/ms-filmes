bf2787bdebf740e8d7153356175b60333bd6420e
igorhms <igorhms@hotmail.com>
2021-11-15 18:07:14 -0300

    Criando RestController e Model para Catalog service

---
+++ movie-catalog-service/src/main/java/com/iftm/mscatalogservice/models/CatalogItem.java
@@ -0,0 +1,13 @@
+package com.iftm.mscatalogservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CatalogItem {
+	
+	private String name;
+	private String desc;
+	private int rating;
+}
